c5cfd8144a1f579cf0b3c957fc7f6f3765fb1556
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-06-16 11:39:06 -0700

    implement error handling

---
+++ blog-api/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const morgan = require("morgan")
+const {NotFoundError} = require("./utils/errors")
+
+const app = express()
+
+app.use(morgan("tiny"))
+app.use(express.json())
+
+app.use((req,res,next)=>{
+    return next(new NotFoundError())
+})
+
+app.use((error,req,res,next) => {
+    const status = error.status || 500
+    const message = error.message
+
+    return res.status(status).json({
+        error: {message,status},
+    })
+
+})
+const port = 3002
+
+app.listen(port,() => {
+console.log(`Server listening on port ${port}`)
+})
